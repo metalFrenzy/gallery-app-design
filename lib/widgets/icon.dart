992d83471e871d25948aa5c1266fd69e87b0f3ac
ahmed emad <ahmed.0881100@gmail.com>
2023-04-12 12:21:31 +0200

    finale

--- lib/widgets/icon.dart
+++ lib/widgets/icon.dart
@@ -6,12 +6,11 @@ class IconRow extends StatelessWidget {
   final List<Color> gradColors;
   final Color color;
 
-  const IconRow({
-    required this.text,
-    required this.image,
-    required this.gradColors,
-    required this.color
-  });
+  const IconRow(
+      {required this.text,
+      required this.image,
+      required this.gradColors,
+      required this.color});
 
   @override
   Widget build(BuildContext context) {
@@ -26,18 +25,19 @@ class IconRow extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Container(
-            padding: EdgeInsets.all(10),
+            padding: const EdgeInsets.all(10),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                gradient: LinearGradient(
-                  colors: gradColors,
+              borderRadius: BorderRadius.circular(15),
+              gradient: LinearGradient(
+                colors: gradColors,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 3,
+                  color: color,
                 ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 3,
-                    color: color
-                  )
-                ]),
+              ],
+            ),
             child: Image.asset(
               image,
               height: 15,
